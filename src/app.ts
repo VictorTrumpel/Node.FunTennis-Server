4843b55d0b980a4cc3789e57178b7b1821897358
Victor <vtrumpel@xdsoft.ru>
2022-05-20 00:35:16 +0300

    [*] add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 import router from "./router/router";
 import { connectToDb } from "@middleware/connecToDb";
 
@@ -9,6 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(connectToDb);
+app.use(
+  cors({
+    origin: process.env["CLIENT_URL"],
+  })
+);
 app.use(router);
 
 app.listen(process.env["PORT"], () => {
