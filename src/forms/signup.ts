4bab1770a0041e95b13cd875773317ab63cf138c
Victor <vtrumpel@xdsoft.ru>
2022-05-24 00:29:44 +0300

    [+] add user some fields

--- src/forms/signup.ts
+++ src/forms/signup.ts
@@ -8,6 +8,12 @@ export type SignupFields = {
 const signupSchema = yup.object().shape({
   username: yup.string().required(),
   password: yup.string().required(),
+  fullName: yup.string().nullable(),
+  balance: yup.number().required(),
+  level: yup.number().required(),
+  role: yup.string().required(),
+  description: yup.string().nullable(),
+  email: yup.string().nullable(),
 });
 
 export const signupValidate = async (userData: SignupFields) => {
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,5 +3,11 @@ import { ObjectId } from "mongodb";
 export type User = {
   _id: ObjectId;
   username: string;
+  fullName?: string;
+  balance: number;
+  level: number;
+  email?: string;
+  role: "student" | "coach";
+  description?: string;
   password: string;
 };
